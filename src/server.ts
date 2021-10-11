2f9987bc2eb41cce99f0b20fc24bce2522af4852
daniphant <daniphnt_dev@yahoo.com>
2021-10-11 16:09:36 -0300

    feat: graphql hello world

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,23 @@
 import express from 'express';
+import {graphqlHTTP} from 'express-graphql';
+
+import {GraphQLSchema, GraphQLObjectType, GraphQLString} from 'graphql';
+
 const app = express();
+
+const schema = new GraphQLSchema({
+	query: new GraphQLObjectType({
+		name: 'HelloWorld',
+		fields: () => ({
+			message: {type: GraphQLString, resolve: () => 'Hello World'},
+		}),
+	}),
+});
+
+app.use('/graphql', graphqlHTTP({
+	graphiql: true,
+	schema,
+}));
 
 app.listen(3000, () => {
 	console.log('Server started on port 3000');

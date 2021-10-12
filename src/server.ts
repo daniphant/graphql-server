1750cc2d817f89b65a657222d648a0536eb05854
daniphant <daniphnt_dev@yahoo.com>
2021-10-12 11:31:33 -0300

    feat: create addBook mutation

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,7 @@
 import express from 'express';
 import {graphqlHTTP} from 'express-graphql';
 
-import {GraphQLSchema, GraphQLObjectType, GraphQLList, GraphQLInt} from 'graphql';
+import {GraphQLSchema, GraphQLObjectType, GraphQLList, GraphQLInt, GraphQLString, GraphQLNonNull} from 'graphql';
 import {authors, books} from './config/constants';
 import {AuthorType, BookType} from './config/types';
 
@@ -40,8 +40,29 @@ const RootQueryType = new GraphQLObjectType({
 	}),
 });
 
+const RootMutationType = new GraphQLObjectType({
+	name: 'Mutation',
+	description: 'Root mutation',
+	fields: () => ({
+		addBook: {
+			type: BookType,
+			description: 'Add a book',
+			args: {
+				name: {type: new GraphQLNonNull(GraphQLString)},
+				authorId: {type: new GraphQLNonNull(GraphQLInt)},
+			},
+			resolve: (_, args) => {
+				const book = {id: books.length + 1, name: args.name, authorId: args.authorId};
+				books.push(book);
+				return book;
+			},
+		},
+	}),
+});
+
 const schema = new GraphQLSchema({
 	query: RootQueryType,
+	mutation: RootMutationType,
 });
 
 app.use('/graphql', graphqlHTTP({
